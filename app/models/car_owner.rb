6fa1c343d207b34784a0a1d0042d958eda55d6bc
gortron <gordon.lanza@gmail.com>
2019-10-15 09:44:36 +0100

    solved the lab by building out methods for carowner, car, mechanic, and writing seed data for testing

--- app/models/car_owner.rb
+++ app/models/car_owner.rb
@@ -1,10 +1,27 @@
 class CarOwner
 
   attr_reader :name
+  @@all = []
 
   def initialize(name)
     @name = name
+    @@all << self
   end
 
+  def self.all
+    @@all
+  end
+
+  def cars
+    Car.all.select { |car| car.owner == self }
+  end
+
+  def mechanics
+    self.cars.map(&:mechanics)
+  end
+
+  def cars_per_owner
+    self.all.map(&:cars).count / @@all.length
+  end
 
 end
